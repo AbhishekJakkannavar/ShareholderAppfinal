6204dee175758dce27e733d47d88dad3ce9d0445
Abhishek Jakkannavar <tracerabhi96@ymail.com>
2018-01-16 14:24:06 +0530

    Home Layout Done.

--- app/src/main/java/shareholder/infinity/com/shareholderapp/MainActivity.java
+++ app/src/main/java/shareholder/infinity/com/shareholderapp/MainActivity.java
@@ -1,13 +1,31 @@
 package shareholder.infinity.com.shareholderapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageButton companybutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //To send intent to CompanyProfile Activity
+        companybutton = findViewById(R.id.companybutton);
+        companybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, CompanyProfile.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
